95159396a04372a2e8d6bbdea1b72522be138a10
Pawas Pandey <pawaspy2633@gmail.com>
2023-10-28 22:59:16 +0530

    Added- README and report file

---
+++ README.md
@@ -0,0 +1,53 @@
+# NSSP Suicidal Risk Prediction Project :sos:
+
+This project is about predicting the suicidal risk of individuals based on various features like gender, age, and previous attempts using machine learning models.
+
+## Getting Started :rocket:
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites :clipboard:
+
+What things you need to install the software and how to install them
+
+`python==3.8 pandas==1.3.3 numpy==1.21.2 matplotlib==3.4.3 seaborn==0.11.2 scikit-learn==1.0`
+
+### Installing :wrench:
+
+A step by step series of examples that tell you how to get a development env running
+
+1. Clone the repo
+`git clone https://github.com/pawaspy/Suicide-Risk.git`
+
+2. Install Python packages
+`pip install -r requirements.txt`
+
+## Running the tests :computer:
+
+Explain how to run the automated tests for this system
+
+`python3 suicide_report.py`
+
+
+## Dataset :file_folder:
+
+The dataset used in this project is the NSSP dataset. Unfortunately, I can't provide a direct link to the dataset as it's not specified where it's located. You might want to check with the data provider or the institution that has the data.
+
+## Built With :hammer_and_wrench:
+
+* [Python](https://www.python.org/)
+* [Pandas](https://pandas.pydata.org/)
+* [Numpy](https://numpy.org/)
+* [Matplotlib](https://matplotlib.org/)
+* [Seaborn](https://seaborn.pydata.org/)
+* [Scikit-Learn](https://scikit-learn.org/stable/)
+
+## Contributing :handshake:
+
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
---
+++ suicide_report.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV, HalvingGridSearchCV, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+
+# Load the NSSP dataset
+df = pd.read_csv('nssp_dataset.csv')
+
+# Clean and process the data
+df.dropna(inplace=True)
+df.drop_duplicates(inplace=True)
+
+# Encode the gender variable
+df['gender'] = df['gender'].map({'female': 0, 'male': 1})
+
+# Create new features
+df['age_in_years_since_18'] = df['age'] - 18
+
+# Define the features and target variable
+features = ['gender', 'age_in_years_since_18', 'previous_attempts']
+target = 'suicidal_risk'
+
+X = df[features]
+y = df[target]
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# Define the models to use
+models = [
+<<<<<<< HEAD
+    ('Logistic Regression', LogisticRegression(), {'C': [0.1, 1, 10], 'penalty': ['l1', 'l2', 'elasticnet', 'none']}),
+    ('Decision Tree', DecisionTreeClassifier(), {'max_depth': [3, 5, 7]}),
+    ('Random Forest', RandomForestClassifier(), {'n_estimators': [50, 100, 200], 'max_depth': [3, 5, 7]})
+]
+
+# Train and evaluate each model
+for name, model, params in models:
+    print('Training', name)
+    
+    # Hyperparameter tuning using grid search and cross-validation
+    grid_search = HalvingGridSearchCV(model, params, cv=5, scoring='accuracy')
+=======
+    ('Logistic Regression', LogisticRegression()),
+    ('Decision Tree', DecisionTreeClassifier()),
+    ('Random Forest', RandomForestClassifier())
+]
+
+# Define the hyperparameters to tune for each model
+params = [
+    {'C': [0.1, 1, 10], 'penalty': ['l1', 'l2']},
+    {'max_depth': [3, 5, 7]},
+    {'n_estimators': [50, 100, 200], 'max_depth': [3, 5, 7]}
+]
+
+# Train and evaluate each model
+for name, model in models:
+    print('Training', name)
+    
+    # Hyperparameter tuning using grid search and cross-validation
+    grid_search = GridSearchCV(model, params, cv=5, scoring='accuracy')
+>>>>>>> e50fb48 (Add-Suicide report)
+    grid_search.fit(X_train, y_train)
+    
+    print('Best hyperparameters:', grid_search.best_params_)
+    
+    # Evaluate the model on the test set
+    y_pred = grid_search.predict(X_test)
+    
+    # Calculate the performance metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    
+    # Print the performance metrics
+    print('Accuracy:', accuracy)
+    print('Precision:', precision)
+    print('Recall:', recall)
+    print('F1 score:', f1)
+    
+    # Generate classification report and confusion matrix
+    print('Classification Report:')
+    print(classification_report(y_test, y_pred))
+    
+    print('Confusion Matrix:')
+    sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, cmap='Blues')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.show()
+    
+<<<<<<< HEAD
+    print('\n')
+=======
+    print('\n')
+>>>>>>> e50fb48 (Add-Suicide report)
